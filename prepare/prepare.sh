78579b68bf129ca5b96be0f3645d0a3c7c2e6442
Takuya Niita <takuya2230@gmail.com>
2022-04-05 17:47:02 +0900

    modify settings

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #Get CompartmentID
 oci iam compartment list | grep compartment-id > compartment-id.txt ; cut -f 2 -d ":" compartment-id.txt | tr -d ' ','"',',' | tee compartment-id-tee.txt &>/dev/null ; compartment_id=`cat ./compartment-id-tee.txt` ; rm -rf ./compartment-id* ; echo ${compartment_id}
+
+#Create Dynamic-Group
+oci iam dynamic-group create --name OCI_DevOps_Dynamic_Group_Compute --description OCI_DevOps_Dynamic_Group_Compute --matching-rule "instance.compartment.id = '${compartment_id}'"
+
 #Create Policy
-oci iam policy create --name OCI_DevOps_Policy_Compute --description OCI_DevOps_Policy_Compute --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group to manage instance-agent-command-execution-family in compartment id '${compartment_id}'","Allow dynamic-group OCI_DevOps_Dynamic_Group to manage instance-family in compartment id '${compartment_id}'"]'
+oci iam policy create --name OCI_DevOps_Policy_Compute --description OCI_DevOps_Policy_Compute --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group_Compute to manage instance-agent-command-family in compartment id '${compartment_id}'"]'
