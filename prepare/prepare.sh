7d31af86a58da0a7af81817d23d53f4a7e16be69
Takuya Niita <takuya.niita@oracle.com>
2022-04-05 20:37:04 +0900

    fixed bug

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -6,4 +6,4 @@ oci iam compartment list | grep compartment-id > compartment-id.txt ; cut -f 2 -
 oci iam dynamic-group create --name OCI_DevOps_Policy_Compute --description OCI_DevOps_Policy_Compute --matching-rule "any {instance.compartment.id = '${compartment_id}'}"
 
 #Create Policy
-oci iam policy create --name OCI_DevOps_Policy_Compute --description OCI_DevOps_Policy_Compute --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group_Compute to manage all-resources in compartment id '${compartment_id}', "Allow dynamic-group OCI_DevOps_Dynamic_Group_Compute to to manage instance-agent-command-family in compartment '${compartment_id}'"]'
+oci iam policy create --name OCI_DevOps_Policy_Compute --description OCI_DevOps_Policy_Compute --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group_Compute to manage all-resources in compartment id '${compartment_id}'", "Allow dynamic-group OCI_DevOps_Dynamic_Group_Compute to to manage instance-agent-command-family in compartment '${compartment_id}'"]'
